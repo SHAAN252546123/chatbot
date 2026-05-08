f26b97fc13630d253a63938cc2ab12630e20d9cf
SHAAN252546123 <shanthanu1405@gmail.com>
2026-05-09 00:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import wikipedia
 
 # ---------------- CONFIG ----------------
 st.set_page_config(
-    page_title="ChatEasy",
+    page_title="NAKKAN CHAT BOT ",
     page_icon="💬",
     layout="wide"
 )
